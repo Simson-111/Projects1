070d8aae28fe130f1da4ccc4f5307920dd4861a3
Simson Vasanth Raj <simsonvasanthraj@gmail.com>
2026-08-14 19:45:51 +0530

    TAx

---
+++ Taxform.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<h1>Tax Invoice For Supply of Goods</h1>
+        <table border={2}>
+            <thead>Raj Enterprices</thead>
+              <tbody>
+                <th>
+                   <p>111 Mullai Nagar,salem junction<br></br>
+                   salem-636 005,Tamil nadu<br></br>GSTIN:33adyapa0048izd<br></br>
+                   MSME NO:Udayam-TN-20-0010336<br></br>E-Mail:rajendran2002@yahoo.co.in<br><br/>
+                   Phone:807288423,9443244802</p> 
+                </th>
+                <th>
+                	<label>Invoice no:</label>
+                	<input type="text" name="">
+                	<label>Date</label>
+                	<input type="text" name="">
+                </th>
+            </tbody>
+     </table>
+</body>
+</html>
